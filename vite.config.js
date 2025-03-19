daaac81f0f90bc939ae238044b75a90f22329d42
Vishal Iyer <iyervish@gmail.com>
2025-03-19 12:28:12 -0400

    Fix GitHub Pages configuration

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
-export default {
+import { defineConfig } from 'vite'
+
+export default defineConfig({
     server: {
         open: true
     },
+    base: '/minecraft-game/',
     build: {
         outDir: 'dist',
         assetsDir: 'assets'
     }
-} 
+}) 
